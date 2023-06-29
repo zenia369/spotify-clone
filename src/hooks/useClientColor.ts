593155235383422159dac2e2cf88060195a3548e
Kushnir E <kyshnirxeneea@gmail.com>
2023-06-29 22:03:18 +0300

    refactor: useClientColor hook

--- src/hooks/useClientColor.ts
+++ src/hooks/useClientColor.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useState, useLayoutEffect } from 'react'
+import { useState, useLayoutEffect, useRef } from 'react'
 
 const colors = [
   'red',
@@ -18,9 +18,14 @@ const getColor = () => colors[Math.floor(Math.random() * colors.length)]
 export default () => {
   const router = useRouter()
   const [color, setColor] = useState(getColor)
+  const colorRef = useRef<boolean>(false)
 
+  if (typeof window === 'undefined') return
   useLayoutEffect(() => {
-    setColor(getColor)
+    if (colorRef.current) {
+      setColor(getColor)
+    }
+    colorRef.current = true
   }, [router.asPath])
 
   return color
